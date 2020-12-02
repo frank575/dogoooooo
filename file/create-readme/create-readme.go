155828ccb0097e008d98e4d0906d8067c1a61a2f
吳俊緯 <wcw850710@gmail.com>
2020-12-02 18:04:21 +0800

    update

--- file/create-readme/create-readme.go
+++ file/create-readme/create-readme.go
@@ -8,6 +8,11 @@ import (
 	"os"
 )
 
+type Info struct {
+	path string
+	info string
+}
+
 func getIgnoreFile() []string {
 	f, err := os.Open(".gitignore")
 	util.CheckOpen(err)
@@ -35,20 +40,25 @@ func checkIgnore(ignoreList *[]string, fileName *string) bool {
 	return isReplace
 }
 
-func createTOC(ignoreList *[]string, title *string, path string, tab string) {
+func createTOC(ignoreList *[]string, title *string, path string, tab string, infoList *[]Info) {
 	//f, err := os.Getwd()
 	//util.CheckGetwd(err)
 
 	dirList, err := ioutil.ReadDir(path)
 	util.CheckReadDir(err)
 
+	info := ""
+
 	for i := range dirList {
 		file := dirList[i]
 		fileName := file.Name()
+		if fileName == "info.txt" {
+			*infoList = append(*infoList, Info{path, "2123132132"})
+		}
 		if file.IsDir() && !checkIgnore(&*ignoreList, &fileName) {
 			path := fmt.Sprintf("%s/%s", path, fileName)
-			*title += fmt.Sprintf("%s- [%s](%s)\n", tab, fileName, path)
-			createTOC(&*ignoreList, &*title, path, tab+"  ")
+			*title += fmt.Sprintf("%s- [%s](%s) %s\n", tab, fileName, path, info)
+			createTOC(&*ignoreList, &*title, path, tab+"  ", &*infoList)
 			//fmt.Println(path, fileName)
 		}
 	}
@@ -61,14 +71,17 @@ func main() {
 	w := bufio.NewWriter(f)
 	title := "# 狗語言練習範例\n" +
 		"```command\n" +
-		"# 生成 README.md 指令\n" +
+		"# 生成 README.md 指令(開發中)\n" +
 		"go run file/create-readme/create-readme.go\n" +
 		"```\n" +
 		"## 目錄結構\n" +
 		"\n"
 
 	ignoreList := getIgnoreFile()
-	createTOC(&ignoreList, &title, ".", "")
+	var infoList []Info
+	createTOC(&ignoreList, &title, ".", "", &infoList)
+
+	fmt.Println(infoList)
 
 	w.WriteString(fmt.Sprintf(title))
 	w.Flush()
